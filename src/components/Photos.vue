dba2a82ef90258febbe91735675dce196b54b982
Tim Garrity <tag.garrity@gmail.com>
2019-01-09 11:42:25 -0700

    give photo pages a link back to main

--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,11 +1,14 @@
 <template>
-  <section class="photo-container">
-    <article 
-      v-for="photo in photos"
-      class="img"
-      v-bind:style="{ backgroundImage: 'url(' + photo.baseimageurl + ')'}"
-    >
-    </article>
+  <section>
+    <router-link to="/">Back to Collections</router-link>
+    <section class="photo-container">
+      <article 
+        v-for="photo in photos"
+        class="img"
+        v-bind:style="{ backgroundImage: 'url(' + photo.baseimageurl + ')'}"
+      >
+      </article>
+    </section>
   </section>
 </template>
 
